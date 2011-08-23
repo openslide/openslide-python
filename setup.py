b015cfd7a84e600b69486066f8f51e9bea502948
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-08-23 15:21:46 -0400

    Add initial infrastructure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import os
+
+# Load version string
+_verfile = os.path.join(os.path.dirname(__file__), 'openslide', '_version.py')
+exec open(_verfile)
+
+setup(
+    name = 'openslide-python',
+    version = __version__,
+    packages = [
+        'openslide',
+    ],
+    author = 'OpenSlide project',
+    author_email = 'openslide-users@lists.andrew.cmu.edu',
+    description = 'Python bindings for OpenSlide library',
+    license = 'GNU Lesser General Public License, version 2.1',
+    keywords = 'openslide whole-slide image library',
+    url = 'http://openslide.org/',
+)
