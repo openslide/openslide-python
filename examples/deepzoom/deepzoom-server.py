6e18fe19926c721c6794bb4e697b5641963390b7
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-09-01 18:42:25 -0400

    Add example Deep Zoom dynamic tiler

---
+++ examples/deepzoom/deepzoom-server.py
@@ -0,0 +1,153 @@
+#!/bin/env python
+#
+# deepzoom-server - Example web application for serving whole-slide images
+#
+# Copyright (c) 2010-2011 Carnegie Mellon University
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of version 2.1 of the GNU Lesser General Public License
+# as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from cStringIO import StringIO
+from flask import Flask, abort, make_response, render_template, url_for
+from openslide import ImageSlide, open_slide
+from openslide.deepzoom import DeepZoomGenerator
+from optparse import OptionParser
+import re
+from unicodedata import normalize
+
+DEEPZOOM_SLIDE = None
+DEEPZOOM_FORMAT = 'jpeg'
+DEEPZOOM_TILE_SIZE = 256
+DEEPZOOM_OVERLAP = 1
+SLIDE_NAME = 'slide'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('DEEPZOOM_TILER_SETTINGS', silent=True)
+
+
+# With Flask 0.8 and above:
+# @app.before_first_request
+def load_slide():
+    slidefile = app.config['DEEPZOOM_SLIDE']
+    if slidefile is None:
+        raise ValueError('No slide file specified')
+    config_map = {
+        'DEEPZOOM_TILE_SIZE': 'tile_size',
+        'DEEPZOOM_OVERLAP': 'overlap',
+    }
+    opts = dict((v, app.config[k]) for k, v in config_map.iteritems())
+    slide = open_slide(slidefile)
+    app.slides = {
+        SLIDE_NAME: DeepZoomGenerator(slide, **opts)
+    }
+    app.associated_images = []
+    app.slide_properties = slide.properties
+    for name, image in slide.associated_images.iteritems():
+        app.associated_images.append(name)
+        slug = slugify(name)
+        app.slides[slug] = DeepZoomGenerator(ImageSlide(image), **opts)
+
+
+@app.route('/')
+def index():
+    slide_url = url_for('dzi', slug=SLIDE_NAME)
+    associated_urls = dict((name, url_for('dzi', slug=slugify(name)))
+            for name in app.associated_images)
+    return render_template('index.html', slide_url=slide_url,
+            associated=app.associated_images, associated_urls=associated_urls,
+            properties=app.slide_properties)
+
+
+@app.route('/<slug>.dzi')
+def dzi(slug):
+    format = app.config['DEEPZOOM_FORMAT']
+    try:
+        resp = make_response(app.slides[slug].get_dzi(format))
+        resp.mimetype = 'application/xml'
+        return resp
+    except KeyError:
+        # Unknown slug
+        abort(404)
+
+
+@app.route('/<slug>_files/<int:level>/<int:col>_<int:row>.<format>')
+def tile(slug, level, col, row, format):
+    format = format.lower()
+    if format != 'jpeg' and format != 'png':
+        # Not supported by Deep Zoom
+        abort(404)
+    try:
+        tile = app.slides[slug].get_tile(level, (col, row))
+    except KeyError:
+        # Unknown slug
+        abort(404)
+    except ValueError:
+        # Invalid level or coordinates
+        abort(404)
+    buf = StringIO()
+    tile.save(buf, format, optimize=True, quality=75)
+    resp = make_response(buf.getvalue())
+    resp.mimetype = 'image/%s' % format
+    return resp
+
+
+# Based on Flask snippet 5
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+def slugify(text, delim=u'-'):
+    """Generates an ASCII-only slug."""
+    result = []
+    for word in _punct_re.split(unicode(text, 'UTF-8').lower()):
+        word = normalize('NFKD', word).encode('ascii', 'ignore')
+        if word:
+            result.append(word)
+    return unicode(delim.join(result))
+
+
+if __name__ == '__main__':
+    parser = OptionParser(usage='Usage: %prog [options] [slide]')
+    parser.add_option('-c', '--config', metavar='FILE', dest='config',
+                help='config file')
+    parser.add_option('-d', '--debug', dest='DEBUG', action='store_true',
+                help='run in debugging mode (insecure)')
+    parser.add_option('-e', '--overlap', metavar='PIXELS',
+                dest='DEEPZOOM_OVERLAP', type='int',
+                help='overlap of adjacent tiles [1]')
+    parser.add_option('-f', '--format', metavar='{jpeg|png}',
+                dest='DEEPZOOM_FORMAT',
+                help='image format for tiles [jpeg]')
+    parser.add_option('-l', '--listen', metavar='ADDRESS', dest='host',
+                default='127.0.0.1',
+                help='address to listen on [127.0.0.1]')
+    parser.add_option('-s', '--size', metavar='PIXELS',
+                dest='DEEPZOOM_TILE_SIZE', type='int',
+                help='tile size [256]')
+
+    (opts, args) = parser.parse_args()
+    # Load config file if specified
+    if opts.config is not None:
+        app.config.from_pyfile(opts.config)
+    # Overwrite only those settings specified on the command line
+    for k in dir(opts):
+        if not k.startswith('_') and getattr(opts, k) is None:
+            delattr(opts, k)
+    app.config.from_object(opts)
+    # Set slide file
+    try:
+        app.config['DEEPZOOM_SLIDE'] = args[0]
+    except IndexError:
+        pass
+    load_slide()
+
+    app.run(host=opts.host)
