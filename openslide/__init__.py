d3e824e57618b1f1f9c110fbfb364590fc837815
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2014-01-05 01:22:25 -0500

    Add bounds properties

--- openslide/__init__.py
+++ openslide/__init__.py
@@ -44,6 +44,10 @@ PROPERTY_NAME_BACKGROUND_COLOR = u'openslide.background-color'
 PROPERTY_NAME_OBJECTIVE_POWER  = u'openslide.objective-power'
 PROPERTY_NAME_MPP_X            = u'openslide.mpp-x'
 PROPERTY_NAME_MPP_Y            = u'openslide.mpp-y'
+PROPERTY_NAME_BOUNDS_X         = u'openslide.bounds-x'
+PROPERTY_NAME_BOUNDS_Y         = u'openslide.bounds-y'
+PROPERTY_NAME_BOUNDS_WIDTH     = u'openslide.bounds-width'
+PROPERTY_NAME_BOUNDS_HEIGHT    = u'openslide.bounds-height'
 
 class AbstractSlide(object):
     """The base class of a slide object."""
