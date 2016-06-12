1a411040b63cf032f6e9e269409818a1179d7eef
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2016-06-11 20:03:03 -0500

    setup.py: Add classifiers for newer Python versions

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
     install_requires=[
