d63d015b79ce3400a069a4d08e93e82c17b0ed24
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2024-10-16 09:05:54 -0700

    doc/jekyll_fix: switch from os.path to pathlib
    Path.walk() isn't available before Python 3.12.
    
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- doc/jekyll_fix.py
+++ doc/jekyll_fix.py
@@ -2,6 +2,7 @@
 # openslide-python - Python bindings for the OpenSlide library
 #
 # Copyright (c) 2014 Carnegie Mellon University
+# Copyright (c) 2024 Benjamin Gilbert
 #
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of version 2.1 of the GNU Lesser General Public License
@@ -26,6 +27,7 @@
 from __future__ import annotations
 
 import os
+from pathlib import Path
 
 from sphinx.application import Sphinx
 from sphinx.util import logging
@@ -49,37 +51,33 @@ def remove_path_underscores(app: Sphinx, exception: Exception | None) -> None:
     logger = logging.getLogger(__name__)
     logger.info(bold('fixing pathnames... '), nonl=True)
     # Rewrite references in HTML/JS files
-    for dirpath, _, filenames in os.walk(app.outdir):
+    outdir = Path(app.outdir)
+    for dirpath, _, filenames in os.walk(outdir):
         for filename in filenames:
-            _, ext = os.path.splitext(filename)
-            if ext in REWRITE_EXTENSIONS:
-                path = os.path.join(dirpath, filename)
-                with open(path, encoding='utf-8') as fh:
+            path = Path(dirpath) / filename
+            if path.suffix in REWRITE_EXTENSIONS:
+                with path.open(encoding='utf-8') as fh:
                     contents = fh.read()
                 for old, new in DIRS.items():
                     contents = contents.replace(old + '/', new + '/')
                 for old, new in FILES.items():
                     contents = contents.replace(old, new)
-                with open(path, 'w', encoding='utf-8') as fh:
+                with path.open('w', encoding='utf-8') as fh:
                     fh.write(contents)
     # Move directory contents
     for old, new in DIRS.items():
-        olddir = os.path.join(app.outdir, old)
-        newdir = os.path.join(app.outdir, new)
-        if not os.path.exists(newdir):
-            os.mkdir(newdir)
-        if os.path.isdir(olddir):
-            for filename in os.listdir(olddir):
-                oldfile = os.path.join(olddir, filename)
-                newfile = os.path.join(newdir, filename)
-                os.rename(oldfile, newfile)
-            os.rmdir(olddir)
+        olddir = outdir / old
+        newdir = outdir / new
+        newdir.mkdir(exist_ok=True)
+        if olddir.is_dir():
+            for oldfile in olddir.iterdir():
+                oldfile.rename(newdir / oldfile.name)
+            olddir.rmdir()
     # Move files
     for old, new in FILES.items():
-        oldfile = os.path.join(app.outdir, old)
-        newfile = os.path.join(app.outdir, new)
-        if os.path.isfile(oldfile):
-            os.rename(oldfile, newfile)
+        oldfile = outdir / old
+        if oldfile.is_file():
+            oldfile.rename(outdir / new)
     logger.info('done')
 
 
