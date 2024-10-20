openslide/openslide-python

commit 98c11bd4ca2b3c54cd86459bdc26f2e069e3fa92

Add support for pathlib.Path instances (#123)
`pathlib.Path` instances are commonly used, but the `openslide.OpenSlide`
class was not compatible with the `pathlib.Path` type.  Have `OpenSlide`
stringify paths before passing them to `lowlevel` functions, and update
tests to check this case.

Co-authored-by: Jan Harkes <jaharkes@cs.cmu.edu>

openslide/__init__.py
@@ -150,7 +150,7 @@ class OpenSlide(AbstractSlide):
         """Open a whole-slide image."""
         AbstractSlide.__init__(self)
         self._filename = filename
-        self._osr = lowlevel.open(filename)
+        self._osr = lowlevel.open(str(filename))
 
     def __repr__(self):
         return '%s(%r)' % (self.__class__.__name__, self._filename)
@@ -160,7 +160,7 @@ class OpenSlide(AbstractSlide):
         """Return a string describing the format vendor of the specified file.
 
         If the file format is not recognized, return None."""
-        return lowlevel.detect_vendor(filename)
+        return lowlevel.detect_vendor(str(filename))
 
     def close(self):
         """Close the OpenSlide object."""

tests/__init__.py
@@ -19,6 +19,8 @@
 
 from functools import wraps
 import os
+from pathlib import Path
+
 from PIL import Image
 import unittest
 
@@ -52,4 +54,4 @@ except ValueError:
 
 
 def file_path(name):
-    return os.path.join(os.path.dirname(__file__), name)
+    return Path(__file__).parent / name

commit a65540ee6d022ccc7f851e2b7e4df89188c61cdd

Fix several problems with filename handling
- Starting in 1.2.0, OpenSlide() and OpenSlide.detect_format() have failed
  to accept filename arguments formatted as bytes because str(b'abc') ==
  "b'abc'".  In addition, filename arguments with invalid types (such as
  None) have been stringified and passed to OpenSlide, rather than raising
  an exception during conversion; we even had tests for this (!).

- lowlevel has always encoded filename arguments to UTF-8, but on
  non-Windows it should have used the Python filesystem encoding instead
  (usually UTF-8 but not always).  On Windows, OpenSlide 4.0.0+ expects
  UTF-8 rather than arbitrary bytes.  (OpenSlide < 4.0.0 expects the system
  codepage, which isn't very useful in practice because of its limited
  character set, so we ignore that case for now.)

- Type hints did not allow filename arguments to be bytes, nor did they
  allow os.PathLike subclasses which were not pathlib.Path (such as
  pathlib.PurePath).

Accept str, bytes, or os.PathLike for all filename arguments, and properly
convert them to bytes for OpenSlide.

Fixes: 98c11bd4ca2b ("Add support for pathlib.Path instances (#123)")
Fixes: 564422975d9b ("tests: test passing invalid types to OpenSlide constructor")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

openslide/__init__.py
@@ -26,7 +26,6 @@ This package provides Python bindings for the OpenSlide library.
 from __future__ import annotations
 
 from io import BytesIO
-from pathlib import Path
 from types import TracebackType
 from typing import Iterator, Literal, Mapping, TypeVar
 
@@ -82,7 +81,7 @@ class AbstractSlide:
         return False
 
     @classmethod
-    def detect_format(cls, filename: str | Path) -> str | None:
+    def detect_format(cls, filename: lowlevel.Filename) -> str | None:
         """Return a string describing the format of the specified file.
 
         If the file format is not recognized, return None."""
@@ -189,11 +188,11 @@ class OpenSlide(AbstractSlide):
     operations on the OpenSlide object, other than close(), will fail.
     """
 
-    def __init__(self, filename: str | Path):
+    def __init__(self, filename: lowlevel.Filename):
         """Open a whole-slide image."""
         AbstractSlide.__init__(self)
         self._filename = filename
-        self._osr = lowlevel.open(str(filename))
+        self._osr = lowlevel.open(filename)
         if lowlevel.read_icc_profile.available:
             self._profile = lowlevel.read_icc_profile(self._osr)
 
@@ -201,11 +200,11 @@ class OpenSlide(AbstractSlide):
         return f'{self.__class__.__name__}({self._filename!r})'
 
     @classmethod
-    def detect_format(cls, filename: str | Path) -> str | None:
+    def detect_format(cls, filename: lowlevel.Filename) -> str | None:
         """Return a string describing the format vendor of the specified file.
 
         If the file format is not recognized, return None."""
-        return lowlevel.detect_vendor(str(filename))
+        return lowlevel.detect_vendor(filename)
 
     def close(self) -> None:
         """Close the OpenSlide object."""
@@ -358,7 +357,7 @@ class OpenSlideCache:
 class ImageSlide(AbstractSlide):
     """A wrapper for a PIL.Image that provides the OpenSlide interface."""
 
-    def __init__(self, file: str | Path | Image.Image):
+    def __init__(self, file: lowlevel.Filename | Image.Image):
         """Open an image file.
 
         file can be a filename or a PIL.Image."""
@@ -376,7 +375,7 @@ class ImageSlide(AbstractSlide):
         return f'{self.__class__.__name__}({self._file_arg!r})'
 
     @classmethod
-    def detect_format(cls, filename: str | Path) -> str | None:
+    def detect_format(cls, filename: lowlevel.Filename) -> str | None:
         """Return a string describing the format of the specified file.
 
         If the file format is not recognized, return None."""
@@ -484,7 +483,7 @@ class ImageSlide(AbstractSlide):
         pass
 
 
-def open_slide(filename: str | Path) -> OpenSlide | ImageSlide:
+def open_slide(filename: lowlevel.Filename) -> OpenSlide | ImageSlide:
     """Open a whole-slide or regular image.
 
     Return an OpenSlide object for whole-slide images and an ImageSlide

openslide/lowlevel.py
@@ -2,7 +2,7 @@
 # openslide-python - Python bindings for the OpenSlide library
 #
 # Copyright (c) 2010-2013 Carnegie Mellon University
-# Copyright (c) 2016-2023 Benjamin Gilbert
+# Copyright (c) 2016-2024 Benjamin Gilbert
 #
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of version 2.1 of the GNU Lesser General Public License
@@ -48,6 +48,7 @@ from ctypes import (
     cdll,
 )
 from itertools import count
+import os
 import platform
 from typing import TYPE_CHECKING, Any, Callable, Protocol, TypeVar, cast
 
@@ -56,7 +57,7 @@ from PIL import Image
 from . import _convert
 
 if TYPE_CHECKING:
-    # Python 3.10+ for ParamSpec
+    # Python 3.10+
     from typing import ParamSpec, TypeAlias
 
     from _convert import _Buffer
@@ -194,6 +195,28 @@ class _OpenSlideCache:
         if not obj._as_parameter_:
             raise ValueError("Passing undefined cache object")
         return obj
+
+
+if TYPE_CHECKING:
+    # Python 3.10+
+    Filename: TypeAlias = str | bytes | os.PathLike[Any]
+
+
+class _filename_p:
+    """Wrapper class to convert filename arguments to bytes."""
+
+    @classmethod
+    def from_param(cls, obj: Filename) -> bytes:
+        # fspath and fsencode raise TypeError on unexpected types
+        if platform.system() == 'Windows':
+            # OpenSlide 4.0.0+ requires UTF-8 on Windows
+            obj = os.fspath(obj)
+            if isinstance(obj, str):
+                return obj.encode('UTF-8')
+            else:
+                return obj
+        else:
+            return os.fsencode(obj)
 
 
 class _utf8_p:
@@ -350,14 +373,14 @@ def _wraps_funcs(
 
 
 try:
-    detect_vendor: _Func[[str], str] = _func(
-        'openslide_detect_vendor', c_char_p, [_utf8_p], _check_string
+    detect_vendor: _Func[[Filename], str] = _func(
+        'openslide_detect_vendor', c_char_p, [_filename_p], _check_string
     )
 except AttributeError:
     raise OpenSlideVersionError('3.4.0')
 
-open: _Func[[str], _OpenSlide] = _func(
-    'openslide_open', c_void_p, [_utf8_p], _check_open
+open: _Func[[Filename], _OpenSlide] = _func(
+    'openslide_open', c_void_p, [_filename_p], _check_open
 )
 
 close: _Func[[_OpenSlide], None] = _func(

tests/test_base.py
@@ -45,7 +45,7 @@ class TestLibrary(unittest.TestCase):
             if getattr(attr, '__module__', None) == '__future__':
                 continue
             # ignore random imports
-            if hasattr(ctypes, name) or name in ('count', 'platform'):
+            if hasattr(ctypes, name) or name in ('count', 'os', 'platform'):
                 continue
             self.assertTrue(
                 hasattr(attr, 'available'),

tests/test_imageslide.py
@@ -1,7 +1,7 @@
 #
 # openslide-python - Python bindings for the OpenSlide library
 #
-# Copyright (c) 2016-2023 Benjamin Gilbert
+# Copyright (c) 2016-2024 Benjamin Gilbert
 #
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of version 2.1 of the GNU Lesser General Public License
@@ -19,6 +19,7 @@
 
 from __future__ import annotations
 
+import sys
 import unittest
 
 from PIL import Image
@@ -43,6 +44,21 @@ class TestImageWithoutOpening(unittest.TestCase):
             with ImageSlide(img) as osr:
                 self.assertEqual(osr.dimensions, (300, 250))
                 self.assertEqual(repr(osr), 'ImageSlide(%r)' % img)
+
+    @unittest.skipUnless(
+        sys.getfilesystemencoding() == 'utf-8',
+        'Python filesystem encoding is not UTF-8',
+    )
+    def test_unicode_path(self):
+        path = file_path('😐.png')
+        for arg in path, str(path):
+            self.assertEqual(ImageSlide.detect_format(arg), 'PNG')
+            self.assertEqual(ImageSlide(arg).dimensions, (300, 250))
+
+    def test_unicode_path_bytes(self):
+        arg = str(file_path('😐.png')).encode('UTF-8')
+        self.assertEqual(ImageSlide.detect_format(arg), 'PNG')
+        self.assertEqual(ImageSlide(arg).dimensions, (300, 250))
 
     def test_operations_on_closed_handle(self):
         with Image.open(file_path('boxes.png')) as img:

tests/test_openslide.py
@@ -1,7 +1,7 @@
 #
 # openslide-python - Python bindings for the OpenSlide library
 #
-# Copyright (c) 2016-2023 Benjamin Gilbert
+# Copyright (c) 2016-2024 Benjamin Gilbert
 #
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of version 2.1 of the GNU Lesser General Public License
@@ -60,11 +60,26 @@ class TestSlideWithoutOpening(unittest.TestCase):
         self.assertRaises(
             OpenSlideUnsupportedFormatError, lambda: OpenSlide('setup.py')
         )
-        self.assertRaises(OpenSlideUnsupportedFormatError, lambda: OpenSlide(None))
-        self.assertRaises(OpenSlideUnsupportedFormatError, lambda: OpenSlide(3))
+        self.assertRaises(ArgumentError, lambda: OpenSlide(None))
+        self.assertRaises(ArgumentError, lambda: OpenSlide(3))
         self.assertRaises(
             OpenSlideUnsupportedFormatError, lambda: OpenSlide('unopenable.tiff')
         )
+
+    @unittest.skipUnless(
+        sys.getfilesystemencoding() == 'utf-8',
+        'Python filesystem encoding is not UTF-8',
+    )
+    def test_unicode_path(self):
+        path = file_path('😐.svs')
+        for arg in path, str(path):
+            self.assertEqual(OpenSlide.detect_format(arg), 'aperio')
+            self.assertEqual(OpenSlide(arg).dimensions, (16, 16))
+
+    def test_unicode_path_bytes(self):
+        arg = str(file_path('😐.svs')).encode('UTF-8')
+        self.assertEqual(OpenSlide.detect_format(arg), 'aperio')
+        self.assertEqual(OpenSlide(arg).dimensions, (16, 16))
 
     def test_operations_on_closed_handle(self):
         osr = OpenSlide(file_path('boxes.tiff'))