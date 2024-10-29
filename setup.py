openslide/openslide-python

commit b893ba288c257e895a2f8ca9e95cfe17661587d2

Switch to PEP 621 project metadata
Drop obsolete test_suite and zip_safe settings.  Combine multi-word
keywords.  Add repo and documentation URLs.

PEP 621 support requires setuptools >= 61.

Closes: https://github.com/openslide/openslide-python/issues/167
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

pyproject.toml
@@ -1,0 +1,45 @@
+[project]
+name = "openslide-python"
+maintainers = [
+    {name = "OpenSlide project", email = "openslide-users@lists.andrew.cmu.edu"}
+]
+description = "Python interface to OpenSlide"
+readme = "README.md"
+license = {text = "GNU Lesser General Public License, version 2.1"}
+keywords = ["OpenSlide", "whole-slide image", "virtual slide", "library"]
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Healthcare Industry",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+requires-python = ">= 3.8"
+dependencies = ["Pillow"]
+dynamic = ["version"]
+
+[project.urls]
+Homepage = "https://openslide.org/"
+Documentation = "https://openslide.org/api/python/"
+Repository = "https://github.com/openslide/openslide-python"
+
+[tool.setuptools]
+packages = ["openslide"]
+
+[tool.setuptools.dynamic]
+version = {attr = "openslide._version.__version__"}
+
+[build-system]
+requires = ["setuptools >= 61.0.0"]
+build-backend = "setuptools.build_meta"

setup.py
@@ -1,54 +1,7 @@
-import os
-
 from setuptools import Extension, setup
 
-# Load version string
-_verfile = os.path.join(os.path.dirname(__file__), 'openslide', '_version.py')
-with open(_verfile) as _fh:
-    exec(_fh.read())  # instantiates __version__
-
-with open('README.md') as _fh:
-    _long_description = _fh.read()
-
 setup(
-    name='openslide-python',
-    version=__version__,  # noqa: F821  undefined-name __version__
-    packages=[
-        'openslide',
-    ],
     ext_modules=[
         Extension('openslide._convert', ['openslide/_convert.c']),
     ],
-    test_suite='tests',
-    maintainer='OpenSlide project',
-    maintainer_email='openslide-users@lists.andrew.cmu.edu',
-    description='Python interface to OpenSlide',
-    long_description=_long_description,
-    long_description_content_type='text/markdown',
-    license='GNU Lesser General Public License, version 2.1',
-    keywords='openslide whole-slide image virtual slide library',
-    url='https://openslide.org/',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Healthcare Industry',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-    ],
-    python_requires='>=3.8',
-    install_requires=[
-        'Pillow',
-    ],
-    zip_safe=True,
 )

.gitignore
@@ -1,5 +1,4 @@
 /build
 /dist
-/MANIFEST
 /*.egg-info
 *.pyc

commit 5994c37a87c6262f3827da92f64dbe2daddca554

Fix configuration of package data
When setuptools 61+ is configured via pyproject.toml, it defaults to
include-package-data = true, rather than the historical default of false.
We don't want wheels to include random files from the package directory,
in particular _convert.c.  Disable include-package-data.

In addition, we were inadvertently relying on setuptools 69+ experimental
functionality to include .pyi and py.typed files in the sdist and .pyi
files in wheels.  Explicitly configure this.

Move py.typed package-data declaration from setup.py to pyproject.toml.

Fixes: b893ba288c25 ("Switch to PEP 621 project metadata")
Fixes: a40175e028b5 ("_convert: add type hints")
Fixes: 5f49c0173cd8 ("Export type hints from openslide package")
Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

MANIFEST.in
@@ -1,4 +1,5 @@
 include *.md
+global-include py.typed *.pyi
 recursive-include doc *.py *.rst
 recursive-include examples *.html *.js *.png *.py
 recursive-include tests *.dcm *.png *.py *.svs *.tiff

pyproject.toml
@@ -38,10 +38,14 @@ Documentation = "https://openslide.org/api/python/"
 Repository = "https://github.com/openslide/openslide-python"
 
 [tool.setuptools]
+include-package-data = false
 packages = ["openslide"]
 
 [tool.setuptools.dynamic]
 version = {attr = "openslide._version.__version__"}
+
+[tool.setuptools.package-data]
+openslide = ["py.typed", "*.pyi"]
 
 [tool.black]
 skip-string-normalization = true

setup.py
@@ -21,7 +21,4 @@ setup(
         # tag wheel for Limited API
         'bdist_wheel': {'py_limited_api': 'cp311'} if _abi3 else {},
     },
-    package_data={
-        'openslide': ['py.typed'],
-    },
 )